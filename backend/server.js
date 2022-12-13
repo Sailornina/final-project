39365c45a7754510f2771eaed0637e88acda3be9
NaghmehOkhovat <99764109+NaghmehOkhovat@users.noreply.github.com>
2022-12-13 11:47:56 +0100

    Added components to src/created userSchema and Login/Register endpoints to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,58 @@
+
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+
+const isAuthenticated = async (req, res, next) => {
+  console.log(req);
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken: accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+};
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -18,6 +66,62 @@ app.get("/", (req, res) => {
   res.send("Hi Nasa API!");
 });
 
+app.get("/users", async (req, res) => {
+	  try {
+	    const users = await User.find().limit(20).exec();
+	    res.status(200).json(users)
+	  } catch (error) {
+	    res.status(400).json({ message: "Could not create user" })
+	  }
+	});
+
+	app.post('/register', async (req, res) => {
+		  const { username, email, password } = req.body;
+		  try {
+		    const salt = bcrypt.genSaltSync(); // Create a randomizer to prevent to unhash it
+		    const newUser = await new User({
+		      username,
+		      email,
+		      password: bcrypt.hashSync(password, salt)
+		    }).save();
+		    res.status(201).json({
+		      response: {
+		        userId: newUser._id,
+		        username: newUser.username,
+		        accessToken: newUser.accessToken
+		      },
+		      success: true
+		    });
+		  } catch (error) {
+		    res.status(400).json({ response: error, success: false });
+		  }
+		});
+		
+		app.post('/login', async (req, res) => {
+		  const { username, email, password } = req.body;
+		  try {
+		    const user = await User.findOne({ username, email });
+		    if (user && bcrypt.compareSync(password, user.password, email, user.email)) {
+		      res.status(200).json({
+		        response: {
+		          userId: user._id,
+		          username: user.username,
+		          email: user.email,
+		          accessToken: user.accessToken
+		        },
+		        success: true
+		      });
+		    } else {
+		      res.status(404).json({
+		        response: "Username, email or password doesn't match.",
+		        success: false
+		      });
+		    }
+		  } catch (error) {
+		    res.status(400).json({ response: error, success: false });
+		  }
+		});
+		
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
---
+++ frontend/src/reducers/user.js
@@ -0,0 +1,31 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const user = createSlice({
+//     name: 'user',
+//     initialState: {
+//         username: null,
+//         accessToken: null,
+//         email: null,
+//         userId: null,
+//         error: null
+//     },
+//     reducers: {
+//         setUserId: (store, action) => {
+//             store.userId = action.payload
+//         },
+//         setUsername: (store, action) => {
+//             store.username = action.payload;
+//         },
+//         setAccessToken: (store, action) => {
+//             store.accessToken = action.payload;
+//         },
+//         setEmail: (store, action) => {
+//             store.email = action.payload;
+//         },
+//         setError: (store, action) => {
+//             store.error = action.payload;
+//         },
+//     }
+// });
+
+// export default user;
