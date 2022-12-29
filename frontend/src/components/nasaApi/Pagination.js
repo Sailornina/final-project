5865620e5c79f8fdefa8c74cb4421b0fdb04eabd
Sailornina <sylcardozo.sc@gmail.com>
2022-12-29 11:53:14 +0100

    SearchForm in progress

---
+++ frontend/src/components/nasaApi/Pagination.js
@@ -0,0 +1,10 @@
+// import React, { useState } from "react";
+
+// const Pagination = () => {
+//     const [pageNumber, setPageNumber] = useState([]);
+//     return (
+
+//     );
+// };
+
+// export default Pagination;
---
+++ frontend/src/components/nasaApi/SearchForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useNavigate, useMatch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getNasaImagesByPage } from ".../utils/nasa-api";
+
+const SearchForm = () => {
+    const [query, setQuery] = useState('');
+
+    const accessToken = useSelector((store) => store.user.accessToken);
+
+    const navigate = useNavigate();
+    // const onCurrentRouteMatch = useMatch('/search');
+
+    useEffect(() => {
+		if (accessToken) {
+			navigate("/search");
+		}
+	}, [accessToken, navigate])
+
+    const onQueryChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    return (
+        <div>
+            <div className="search-div">
+                <form className='search-form'>
+                    <input
+                    className='search-input'
+                    placeholder='Search for moon, supernova...'
+                    type="text"
+                    value={query}
+                    onChange={onQueryChange} />
+                    <button className='search-button' type='onSubmit'>Search</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SearchForm;
