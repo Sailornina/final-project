9ba4eb1c15496e2f6b71aed7417069a9475aad67
Sailornina <sylcardozo.sc@gmail.com>
2023-01-08 14:12:24 +0100

    Pagination is working in search bar

--- frontend/src/components/nasaSearch/SearchForm.js
+++ frontend/src/components/nasaSearch/SearchForm.js
@@ -2,24 +2,34 @@ import React from "react";
 import { useState } from "react";
 import { searchNasaImagesByPage } from "../../apis/nasa-api";
 import ImageDetails from "./ImageDetails";
-import PaginationOutlined from "./PaginationOutlined";
+import Pagination from '@mui/material/Pagination';
 
 const SearchForm = () => {
     const [result, setResult] = useState({images: []});
     const [query, setQuery] = useState("");
     const [page, setPage] = useState(1);
 
-    const onClickSearch = async (e) => {
+    const executeSearch = async (query, page) => {
         const imagesDetails = await searchNasaImagesByPage(query, page);
-        console.log(`Result: ${JSON.stringify(imagesDetails)}`)
+        console.log(`Result: ${JSON.stringify(imagesDetails)}`);
         setResult(imagesDetails);
+    }
+
+    const onClickSearch = async (e) => {
+        await executeSearch(query, page);
     };
 
     const updateQuery = (e) => {
         setQuery(e.target.value)
     };
 
-    const onFormSubmit = (e) => e.preventDefault()
+    const onFormSubmit = (e) => e.preventDefault();
+
+    const handleChangePage = async (e, value) => {
+        await executeSearch(query, page);
+        // console.log(`handleChangePage value: ${value}`)
+        setPage(value);
+    };
 
     return (
         <div className="search-container">
@@ -34,11 +44,16 @@ const SearchForm = () => {
                     />
                     <button onClick={onClickSearch}>Search</button>
                 </form>
-                <PaginationOutlined page={page} setPage={setPage} />
+                <Pagination count={5} 
+                    variant="outlined" 
+                    color="secondary"
+                    onChange={handleChangePage}
+                    />
                 {result.images.map((image) => (
                    <ImageDetails
                    key={image.id}
-                   image={image}/>
+                   image={image}
+                   />
                 ))}
             </div>
         </div>
@@ -46,3 +61,5 @@ const SearchForm = () => {
 };
 
 export default SearchForm;
+
+
