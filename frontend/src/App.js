631f5d8aa02bc10424da4c651e02583d3c170680
Sailornina <sylcardozo.sc@gmail.com>
2023-01-08 21:10:55 +0100

    CommentForm.js and CommentList in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Profile from "./components/Profile";
 import SpaceFeed from "./components/community/SpaceFeed";
 import Loading from "./components/Loading";
 import NotFound from "./components/NotFound";
+import CommentForm from "./components/community/CommentForm";
 
 export const App = () => {
 	const reducer = combineReducers({
@@ -38,6 +39,9 @@ export const App = () => {
 					<Route
 						path="/space-feed"
 						element={<SpaceFeed />} />
+					<Route
+						path="/comment-form"
+						element={<CommentForm />} />
 					<Route
 						path="/login"
 						element={<Login />} />
--- frontend/src/components/community/CommentForm.js
+++ frontend/src/components/community/CommentForm.js
@@ -7,36 +7,36 @@ const CommentForm = (id) => {
 	// const [comments, setComments] = useState([]);
 	const accessToken = useSelector((store) => store.user.accessToken);
 
+	const handleFormComment = (e) => {
+		e.preventDefault()
+
+		const comment = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': accessToken
+			},
+			body: { text: newComment }
+		};
+	
+		// console.log(`Comment: ${JSON.stringify(comment)}`)
+	
+		fetch(`https://final-project-w5otwao4va-lz.a.run.app/posts/${id}/comment`, comment)
+			.then((res) => {
+				if (res.status === 200) {
+					res.json()
+						.then((commentedPost) => {
+							// onCommentSubmitted(commentedPost)
+							console.log(`Request successful: ${JSON.stringify(commentedPost)}`)
+						})
+						.then(() => setNewComment(''))
+				}
+			});
+	};
+
 	const handleOnNewComment = (e) => {
 		setNewComment(e.target.value)
 	};
-
-	const onCommentSubmitted = (newComment) => {
-		setNewComment([])
-	};
-
-	const comment = {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': accessToken
-		},
-		body: { text: newComment }
-	};
-
-	// console.log(`Comment: ${JSON.stringify(comment)}`)
-
-	fetch(`https://final-project-w5otwao4va-lz.a.run.app/posts/${id}/comment`, comment)
-		.then((res) => {
-			if (res.status === 200) {
-				res.json()
-					.then((commentedPost) => {
-						onCommentSubmitted(commentedPost)
-						console.log(`Request successful: ${JSON.stringify(commentedPost)}`)
-					})
-					.then(() => setNewComment(''))
-			}
-		});
 		
 	return (
 		<div className="nested-comments">
@@ -47,13 +47,12 @@ const CommentForm = (id) => {
 				name="new-comment"
 				placeholder="add a comment"
 				value={newComment}
-				onChange={handleOnNewComment}
+				onChange={handleFormComment}
 				rows="5"
 				cols="23" />
-			<button type="submit" onClick={onCommentSubmitted}>Post</button>
+			<button type="submit" onClick={handleOnNewComment}>Post</button>
 		</div>
-	)
-
+	);
 };
 
 export default CommentForm;
