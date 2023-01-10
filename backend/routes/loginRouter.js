81459b4998426acd4488c25cdb90775956593ecd
Sailornina <sylcardozo.sc@gmail.com>
2023-01-10 20:15:18 +0100

    email was deleted in loginRouter.js

--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -5,22 +5,21 @@ import bcrypt from "bcrypt";
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-	const { username, email, password } = req.body;
+	const { username, password } = req.body;
 	try {
-		const user = await User.findOne({ username, email });
-		if (user && bcrypt.compareSync(password, user.password, email, user.email)) {
+		const user = await User.findOne({ username });
+		if (user && bcrypt.compareSync(password, user.password, user.email)) {
 			res.status(200).json({
 				response: {
 					userId: user._id,
 					username: user.username,
-					email: user.email,
 					accessToken: user.accessToken
 				},
 				success: true
 			});
 		} else {
 			res.status(404).json({
-				response: "Username, email or password doesn't match.",
+				response: "Username or password doesn't match.",
 				success: false
 			});
 		}
