25e15722bfc868d5278cf6d9ef5f5ef091abc94a
Sailornina <sylcardozo.sc@gmail.com>
2023-01-12 12:35:23 +0100

    nasa-api deploy link was uncommentend

--- frontend/src/apis/nasa-api.js
+++ frontend/src/apis/nasa-api.js
@@ -12,8 +12,8 @@ export const getNasaImages = async (query) => {
 
 export const searchNasaImagesByPage = async (query, page) => {
     try {
-        // const response = await axios.get(`https://final-project-w5otwao4va-lz.a.run.app/images/search?q=${query}&page=${page}`);
-        const response = await axios.get(`http://localhost:8080/images/search?q=${query}&page=${page}`);
+        const response = await axios.get(`https://final-project-w5otwao4va-lz.a.run.app/images/search?q=${query}&page=${page}`);
+        // const response = await axios.get(`http://localhost:8080/images/search?q=${query}&page=${page}`);
         return response.data;
     } catch (error) {
         return console.error("Failed to fetch nasa images", error);
