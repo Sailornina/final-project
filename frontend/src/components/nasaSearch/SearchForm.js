73065413efa032e9013df5ea5ef65f6bf63fd61b
NaghmehOkhovat <99764109+NaghmehOkhovat@users.noreply.github.com>
2023-01-12 14:44:17 +0100

    Added Loading to SearchForm.js

--- frontend/src/components/nasaSearch/SearchForm.js
+++ frontend/src/components/nasaSearch/SearchForm.js
@@ -1,15 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Loading from "../Loading";
 import { searchNasaImagesByPage } from "../../apis/nasa-api";
 import ImageDetails from "./ImageDetails";
 import Pagination from "@mui/material/Pagination";
 
 const SearchForm = () => {
+	const [loading, setLoading] = useState(true);
   const [result, setResult] = useState({ images: [] });
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+
+	useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
 
   const executeSearch = async (query, page) => {
     const imagesDetails = await searchNasaImagesByPage(query, page);
@@ -30,42 +37,47 @@ const SearchForm = () => {
 
   const handleChangePage = async (e, value) => {
     await executeSearch(query, value);
-  
+
     setPage(value);
   };
-  
-    return (
-        <ContainerSearch className="search-container">
-            <div className="search-div">
-                <Form onSubmit={onFormSubmit}>
-                    <InputSearch
-                        className='search-input'
-                        placeholder='Search for moon, supernova...'
-                        type="text"
-                        value={query}
-                        onChange={updateQuery}
-                    />
-                    <ButtonSearch onClick={onClickSearch}>Search</ButtonSearch>
-                </Form>
-                <ResultSearch className="result-search">
-                    {result.images.length > 0 &&
-                        <Pagination
-                            count={totalPages}
-                            variant="outlined"
-                            color="secondary"
-                            onChange={handleChangePage}
-                            sx={{display:"flex", justifyContent:"center"}}
-                        />}
-                    {result.images.map((image) => (
-                        <ImageDetails
-                            key={image.id}
-                            image={image}
-                        />
-                    ))}
-                </ResultSearch>
-            </div>
-        </ContainerSearch>
-    );
+
+  return (
+		<>
+		{loading === false ? (
+    <ContainerSearch className="search-container">
+      <div className="search-div">
+        <Form onSubmit={onFormSubmit}>
+          <InputSearch
+            className="search-input"
+            placeholder="Search for moon, supernova..."
+            type="text"
+            value={query}
+            onChange={updateQuery}
+          />
+          <ButtonSearch onClick={onClickSearch}>Search</ButtonSearch>
+        </Form>
+        <ResultSearch className="result-search">
+          {result.images.length > 0 && (
+            <Pagination
+              count={totalPages}
+              variant="outlined"
+              color="secondary"
+              onChange={handleChangePage}
+              sx={{ display: "flex", justifyContent: "center" }}
+            />
+          )}
+          {result.images.map((image) => (
+            <ImageDetails key={image.id} image={image} />
+          ))}
+        </ResultSearch>
+      </div>
+    </ContainerSearch>
+		    ) : (
+					<Loading />
+					)}
+					</>	
+  );
+
 };
 
 export default SearchForm;
@@ -74,12 +86,13 @@ export const ContainerSearch = styled.div`
   width: 100%;
   height: 100px;
   display: block;
-	margin-top: 20px;
+  margin-top: 20px;
   @media (min-width: 1440px) {
     display: block;
     width: 100%;
     height: 100px;
-    padding: 10px 0px;}
+    padding: 10px 0px;
+  }
 `;
 
 export const Form = styled.form`
@@ -95,20 +108,20 @@ export const InputSearch = styled.input`
   justify-content: center;
   place-items: center;
   width: 60%;
-  border: 3px solid #00B4CC;
+  border: 3px solid #00b4cc;
   border-right: none;
   padding: 5px;
   height: 50px;
   border-radius: 5px 0 0 5px;
   outline: none;
-  color: #3A4F7A;
+  color: #3a4f7a;
 `;
 
 export const ButtonSearch = styled.button`
   width: 150px;
   height: 50px;
-  border: 1px solid #00B4CC;
-  background: #00B4CC;
+  border: 1px solid #00b4cc;
+  background: #00b4cc;
   text-align: center;
   color: #fff;
   border-radius: 0 5px 5px 0;
@@ -121,7 +134,7 @@ export const ResultSearch = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr;
   column-gap: 15px;
-	@media (max-width: 667px) {
+  @media (max-width: 667px) {
     grid-template-columns: 1fr;
-		}
+  }
 `;
