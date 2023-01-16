c36bf95f1b81e64e13bc0ac755d3cc210b69e84d
Sailornina <sylcardozo.sc@gmail.com>
2023-01-16 10:21:52 +0100

    Linkedin profile was added in Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import { FooterText } from "../styles/GlobalStyle";
 
 const Footer = () => {
   return (
-    <FooterText>@ Created by Antonella Cardozo and Naghmeh Okhovat</FooterText>
+    <FooterText>@ Created by <a href="https://www.linkedin.com/in/antonella-cardozo-187562b7/">Antonella Cardozo</a> and <a href="https://www.linkedin.com/in/naghmeh-okhovat-92508378/"> Naghmeh Okhovat.</a></FooterText>
   );
 };
 
