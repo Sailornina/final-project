8d0ae2cc6b98b01ad80be028c637dcdc9c9caf0b
Sailornina <sylcardozo.sc@gmail.com>
2023-01-17 15:27:46 +0100

    styling to links in Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 import { FooterText } from "../styles/GlobalStyle";
 
 const Footer = () => {
   return (
-    <FooterText>@ Created by <a href="https://www.linkedin.com/in/antonella-cardozo-187562b7/">Antonella Cardozo</a> and <a href="https://www.linkedin.com/in/naghmeh-okhovat-92508378/"> Naghmeh Okhovat.</a></FooterText>
+    <FooterText>@ Created by <LinkProfile href="https://www.linkedin.com/in/antonella-cardozo-187562b7/">Antonella Cardozo</LinkProfile> and <LinkProfile href="https://www.linkedin.com/in/naghmeh-okhovat-92508378/"> Naghmeh Okhovat.</LinkProfile></FooterText>
   );
 };
 
 export default Footer;
+
+export const LinkProfile = styled.a`
+color: white;
+`;
